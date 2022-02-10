9e3b750466d28b48052ff64ff746a8a72fc86294
kawgh1 <krseven@gmail.com>
2022-02-10 16:11:00 -0600

    update - changing default user from null to john

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -8,32 +8,36 @@ import AuthReducer from "./AuthReducer";
 // 	error: false,
 // };
 
+// in real life we would set default state user to "null"
+// but since this is for display, I dont want visitors to hit a "login screen" - I want them to immediately see the app as a user
+
 // DEV
 const INITIAL_STATE = {
-	user: JSON.parse(localStorage.getItem("user")) || null,
-	isFetching: false,
-	error: false,
+    // user: JSON.parse(localStorage.getItem("user")) || null,
+    user: JSON.parse(localStorage.getItem("user")) || "john",
+    isFetching: false,
+    error: false,
 };
 
 export const AuthContext = createContext(INITIAL_STATE);
 
 export const AuthContextProvider = ({ children }) => {
-	const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+    const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
-	useEffect(() => {
-		localStorage.setItem("user", JSON.stringify(state.user));
-	}, [state.user]);
+    useEffect(() => {
+        localStorage.setItem("user", JSON.stringify(state.user));
+    }, [state.user]);
 
-	return (
-		<AuthContext.Provider
-			value={{
-				user: state.user,
-				isFetching: state.isFetching,
-				error: state.error,
-				dispatch,
-			}}
-		>
-			{children}
-		</AuthContext.Provider>
-	);
+    return (
+        <AuthContext.Provider
+            value={{
+                user: state.user,
+                isFetching: state.isFetching,
+                error: state.error,
+                dispatch,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
 };
